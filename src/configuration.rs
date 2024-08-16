84fabb36176f1cf357b86b6ade1b2800f51155b9
link-mozy <rnjs213@gmail.com>
2024-08-16 22:53:57 +0900

    chapter 4 done.

---
+++ src/configuration.rs
@@ -0,0 +1,53 @@
+use secrecy::{ExposeSecret, Secret};
+
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+    // 내부 스크릿 값
+    pub password: Secret<String>,
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> Secret<String> {
+        Secret::new(format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username,
+            self.password.expose_secret(),
+            self.host,
+            self.port,
+            self.database_name
+        ))
+    }
+
+    pub fn connection_string_without_db(&self) -> Secret<String> {
+        Secret::new(format!(
+            "postgres://{}:{}@{}:{}",
+            self.username,
+            self.password.expose_secret(),
+            self.host,
+            self.port,
+        ))
+    }
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // 구성 읽기를 초기화한다.
+    let settings = config::Config::builder()
+        // `configuration.yaml`이라는 파일로부터 구성값을 추가한다.
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
+        .build()?;
+    // 읽은 구성값을 Settings 타입으로 변환한다.
+    settings.try_deserialize::<Settings>()
+}
---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,53 @@
+use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+// subscribe: 필요한 루틴을 호출해서 처리할 일을 조율하고, HTTP 프로토콜의 규칙과 관습에 따라
+// 그 결과물을 적절한 응답으로 변환한다.
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
+}
+// insert_subscriber: 데이터베이스 로직을 처리하고, 둘러싸고 있는 웹 프레임워크에는 신경 쓰지 않는다.
+// 즉, 입력 타입으로 web::Form이나 web::Data 래퍼를 전달하지 않는다.
