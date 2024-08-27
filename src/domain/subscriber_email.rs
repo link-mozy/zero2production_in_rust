6a58403c976c1e5e6da124529cb2df9b9ef0dc57
link-mozy <rnjs213@gmail.com>
2024-08-27 23:57:13 +0900

    chapter 6 done.

---
+++ src/domain/subscriber_email.rs
@@ -0,0 +1,64 @@
+use validator::validate_email;
+
+#[derive(Debug)]
+pub struct SubscriberEmail(String);
+
+impl SubscriberEmail {
+    pub fn parse(s: String) -> Result<SubscriberEmail, String> {
+        if validate_email(&s) {
+            Ok(Self(s))
+        } else {
+            Err(format!("{} is not a valid subscriber email.", s))
+        }
+    }
+}
+
+impl AsRef<str> for SubscriberEmail {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // `SafeEmail` 페어커를 임포트한다.
+    // 또한 `Fake` 트레이트를 사용해서 `SafeEmail`의 `.fake` 메서드에 접근한다.
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
+    use super::SubscriberEmail;
+    use claim::assert_err;
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let email = "".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "ursulaomain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email = "@domain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    // `quickcheck`는 `Clone`과 `Debug`가 필요하다.
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn vaild_emails_are_parsed_successfully(vaild_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(vaild_email.0).is_ok()
+    }
+}
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,15 +1,32 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::subscriber;
 use uuid::Uuid;
 // `String`과 `&str`에 `graphemes` 메서드를 제공하기 위한 확장 트레이드
 
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
     email: String,
     name: String,
+}
+
+pub fn parse_subscriber(form: FormData) -> Result<NewSubscriber, String> {
+    let name = SubscriberName::parse(form.name)?;
+    let email = SubscriberEmail::parse(form.email)?;
+    Ok(NewSubscriber { email, name })
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self { email, name })
+    }
 }
 
 #[tracing::instrument(
@@ -24,13 +41,14 @@ pub async fn subscribe(
     form: web::Form<FormData>, 
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+    // let new_subscriber = match parse_subscriber(form.0) {
+    //     Ok(subscriber) => subscriber,
+    //     Err(_) => return HttpResponse::BadRequest().finish(),
+    // };
+    // let new_subscriber = match NewSubscriber::try_from(form.0) { ... }
+    let new_subscriber = match form.0.try_into() {
+        Ok(form) => form,
         Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    let new_subscriber = NewSubscriber {
-        email: form.0.email,
-        name,
     };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
@@ -54,7 +72,7 @@ pub async fn insert_subscriber(
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        new_subscripber.email,
+        new_subscripber.email.as_ref(),
         new_subscripber.name.inner_ref(),
         Utc::now()
     )
