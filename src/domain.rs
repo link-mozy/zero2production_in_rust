e2fd40c7a14268e73aa525aa4a1a38d61cb151ff
link-mozy <rnjs213@gmail.com>
2024-08-27 21:15:57 +0900

    chapter 6 - 6.11 이메일 포맷

--- src/domain.rs
+++ src/domain.rs
@@ -5,6 +5,7 @@ pub struct NewSubscriber {
     pub name: SubscriberName,
 }
 
+#[derive(Debug)]
 pub struct SubscriberName(String);
 
 impl SubscriberName {
@@ -31,22 +32,72 @@ impl SubscriberName {
     /// 입력이 subscriber 이름에 대한 검증 조건을 모두 만족하면
     /// `SubscriberName` 인스턴스를 반환한다.
     /// 그렇지 않으면 패닉에 빠진다.
-    pub fn parse(s: String) -> SubscriberName {
+    pub fn parse(s: String) -> Result<SubscriberName, String> {
         // `.trim()`은 입력 `s`에 대해 뒤에 이어지는 공백 같은 문자를 제외한 뷰를 반환한다.
         // `.is_emty`는 해당 뷰가 문자를 포함하고 있는지 확인한다.
         let is_empty_or_whitespace = s.trim().is_empty();
 
-        let is_tool_long = s.graphemes(true).count() > 256;
+        let is_too_long = s.graphemes(true).count() > 256;
 
         let forbidden_characters = ['/', '(', ')', '<', '>', '\\', '{', '}'];
         let contains_forbidden_characters = s
             .chars()
             .any(|g| forbidden_characters.contains(&g));
 
-        if is_empty_or_whitespace || is_tool_long || contains_forbidden_characters {
-            panic!("{} is not a valid subscriber name", s)
+        if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
+            Err(format!("{} is not a valid subscriber name.", s))
         } else {
-            Self(s)
+            Ok(Self(s))
         }
+    }
+}
+
+impl AsRef<str> for SubscriberName {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::SubscriberName;
+    use claim::{assert_err, assert_ok};
+
+    #[test]
+    fn a_256_grapheme_long_name_is_valid() {
+        let name = "a̐".repeat(256);
+        assert_ok!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn a_name_longer_than_256_graphemes_is_rejected() {
+        let name = "a".repeat(257);
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn whitespace_only_names_are_rejected() {
+        let name = " ".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let name = "".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn names_containing_an_invalid_character_are_rejected() {
+        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}'] {
+            let name = name.to_string();
+            assert_err!(SubscriberName::parse(name));
+        }
+    }
+
+    #[test]
+    fn a_valid_name_is_parsed_successfully() {
+        let name = "ursula Le Guin".to_string();
+        assert_ok!(SubscriberName::parse(name));
     }
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,7 +3,6 @@ use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
 // `String`과 `&str`에 `graphemes` 메서드를 제공하기 위한 확장 트레이드
-use unicode_segmentation::UnicodeSegmentation;
 
 use crate::domain::{NewSubscriber, SubscriberName};
 
@@ -25,16 +24,15 @@ pub async fn subscribe(
     form: web::Form<FormData>, 
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    // 'web::Form'은 'FormData'의 래퍼다.
-    // 'form.0'을 사용하면 기반 'FormData'에 접근할 수 있다.
-    let new_subscripber = NewSubscriber {
-        email: form.0.email,
-        name: SubscriberName::parse(form.0.name),
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(name) => name,
+        Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    // if !is_valid_name(&form.name) {
-    //     return HttpResponse::BadRequest().finish();
-    // }
-    match insert_subscriber(&pool, &new_subscripber).await {
+    let new_subscriber = NewSubscriber {
+        email: form.0.email,
+        name,
+    };
+    match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
@@ -67,19 +65,4 @@ pub async fn insert_subscriber(
         e
     })?;
     Ok(())
-}
-// insert_subscriber: 데이터베이스 로직을 처리하고, 둘러싸고 있는 웹 프레임워크에는 신경 쓰지 않는다.
-// 즉, 입력 타입으로 web::Form이나 web::Data 래퍼를 전달하지 않는다.
-
-/// 입력이 subscriber 이름에 대한 검증 제약 사항을 모두 만족하면 'true'를 반환한다.
-/// 그렇지 않으면 'false'를 반환한다.
-pub fn is_valid_name(s: &str) -> bool {
-    let is_empty_or_whitespace = s.trim().is_empty();
-    let is_too_long = s.graphemes(true).count() > 256;
-    let forbidden_charactes = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
-    let contains_forbidden_characters = s
-        .chars()
-        .any(|g| forbidden_charactes.contains(&g));
-
-    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 }
